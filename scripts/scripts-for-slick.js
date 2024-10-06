287ddb01966a85d39b2ebeecf0d972db7a4dfee2
yourunb <bxyura@gmail.com>
2024-10-06 14:23:40 +0300

    feat: add slider and images

---
+++ scripts/scripts-for-slick.js
@@ -0,0 +1,41 @@
+$(document).ready(function(){
+  $('.slider').slick({
+    autoplay: 3000,
+    dots: true,
+    arrows: true,
+    infinite: true,
+  });
+});
+
+$('.multiple-slider').slick({
+autoplay: 2000,
+swipeToSlide: true,
+infinite: true,
+speed: 1000,
+slidesToShow: 2,
+slidesToScroll: 1,
+responsive: [
+  {
+    breakpoint: 1024,
+    settings: {
+      slidesToShow: 3,
+      slidesToScroll: 1,
+      infinite: true,
+    }
+  },
+  {
+    breakpoint: 600,
+    settings: {
+      slidesToShow: 2,
+      slidesToScroll: 1
+    }
+  },
+  {
+    breakpoint: 480,
+    settings: {
+      slidesToShow: 1,
+      slidesToScroll: 1
+    }
+  }
+]
+});
