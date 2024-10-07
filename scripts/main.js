e955b94cb9a07472c5d50c8ee5e2299967f6768c
yourunb <bxyura@gmail.com>
2024-10-07 15:15:39 +0300

    feat: complete news page

--- scripts/main.js
+++ scripts/main.js
@@ -2,9 +2,11 @@
 const activity = document.querySelector('.activity-box');
 const activityItems = document.querySelectorAll('.activity-list__item');
 
-activity.addEventListener('click', (e) => {
- if (e.target.classList.contains('activity-list__item')) (e.target.classList.toggle('activity-list__item_checked'))
-})
+if (activity) {
+  activity.addEventListener('click', (e) => {
+    if (e.target.classList.contains('activity-list__item')) (e.target.classList.toggle('activity-list__item_checked'))
+  });
+}
 
 //------------------------------------управление меню
 const btnOpenMenu = document.querySelector('#btn-open-menu');
